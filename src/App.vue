c0450f79cc808535e8faeb8a511e747ddf7cd54b
이지원 <zylee503@naver.com>
2026-07-31 17:47:17 +0900

    0731_실습1 mockup올리기

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WeatherPage from './views/WeatherPage.vue'
+
+// import SampleOne from './views/SampleOne.vue'
+</script>
+<template>
+  <div style="padding: 20px">
+    <!-- <SampleOne /> -->
+    <WeatherPage />
+  </div>
+</template>
